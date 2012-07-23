de37b03c3da62c2717f473bca4e13119479a2da0
Jens Arps <mail@jensarps.de>
2012-07-24 00:34:45 +0200

    Prevent creation of new objects

--- src/RadarDetector.js
+++ src/RadarDetector.js
@@ -10,9 +10,15 @@ define([], function () {
 
   var RadarDetector = function (camera, objects) {
     this.camera = camera;
+
     this.forward = new THREE.Vector3(0, 0, -1);
     this.up = new THREE.Vector3(0, 1, 0);
     this.right = new THREE.Vector3();
+
+    this.forwardVec = new THREE.Vector3(0, 0, -1);
+    this.upVec = new THREE.Vector3(0, 1, 0);
+
+    this.targetVector = new THREE.Vector3();
 
     this.objects = objects;
   };
@@ -28,38 +34,23 @@ define([], function () {
     update: function () {
       var camera = this.camera;
 
-      // set Forward, Right and Up vectors
-      this.forward = camera.quaternion.multiplyVector3(new THREE.Vector3(0, 0, -1)).normalize();
-      this.up = camera.quaternion.multiplyVector3(new THREE.Vector3(0, 1, 0)).normalize();
+      // set Forward, Right and Up plane vectors
+      camera.quaternion.multiplyVector3(this.forwardVec, this.forward).normalize();
+      camera.quaternion.multiplyVector3(this.upVec, this.up).normalize();
       this.right.cross(this.forward, this.up).normalize();
 
-      //console.log(this.forward.x, this.forward.y, this.forward.z);
-
       this.objects.forEach(function (object) {
-        //console.log(object);
-        var position = object.model.position.clone();
 
-        var targetVector = new THREE.Vector3();
-        targetVector.sub(position, camera.position);
-        targetVector.normalize();
-
-
-        //console.log(targetVector.x, targetVector.y, targetVector.z);
-
+        var targetVector = this.targetVector;
+        targetVector.sub(object.model.position, camera.position).normalize();
 
         var isInFront = targetVector.dot(this.forward) > 0;
-        //console.log(isInFront);
 
         var projScalar = targetVector.dot(this.forward) / this.forward.lengthSq();
-        targetVector.subSelf(this.forward.clone().multiplyScalar(projScalar));
+        targetVector.subSelf(this.forward.clone().multiplyScalar(projScalar)); // Can we somehow get rid of having to clone here?
 
-        var radarPos = new THREE.Vector2(
-          targetVector.dot(this.right) / -this.right.lengthSq(),
-          targetVector.dot(this.up) / this.up.lengthSq()
-        );
-
-        var left = radarPos.x * -100; // [-100..100]
-        var top = radarPos.y * -100; // [-100..100]
+        var left = ( targetVector.dot(this.right) / -this.right.lengthSq() ) * -100;
+        var top = ( targetVector.dot(this.up) / this.up.lengthSq() ) * -100;
 
         object.radarLeft = left;
         object.radarTop = top;
