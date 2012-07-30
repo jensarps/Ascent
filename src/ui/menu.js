4119576eabf2d9ee43790cad1d0fd19f6f796050
Jens Arps <mail@jensarps.de>
2012-07-30 11:22:20 +0200

    Style

--- src/ui/menu.js
+++ src/ui/menu.js
@@ -32,7 +32,7 @@ define([
         ]
       },
       options: {
-        items:  [
+        items: [
           { text: 'Toggle Fullscreen', message: 'ui/fullscreen' },
           { text: 'Invert Y-Axis: on', message: 'config/set', data: 'controls.invertYAxis', state: 'on'},
           { text: 'Back', message: 'menu/back' }
@@ -45,18 +45,20 @@ define([
       }
     },
 
-    init: function(parentNode){
+    init: function (parentNode) {
       this.parentNode = parentNode;
       this.history = [];
       this.setupCampaignPage();
       this.show('front');
 
-      comm.subscribe('menu/options', function(){ this.show('options') }.bind(this));
+      comm.subscribe('menu/options', function () {
+        this.show('options')
+      }.bind(this));
       comm.subscribe('menu/show', this.show.bind(this));
       comm.subscribe('menu/back', this.showLast.bind(this));
 
-      this.parentNode.addEventListener('click', function(evt){
-        if(evt.target.classList.contains('menu-item')){
+      this.parentNode.addEventListener('click', function (evt) {
+        if (evt.target.classList.contains('menu-item')) {
           var dataset = evt.target.dataset;
           if (dataset.state) {
             this.toggleState(evt.target);
@@ -67,7 +69,7 @@ define([
       }.bind(this));
     },
 
-    setupCampaignPage:function () {
+    setupCampaignPage: function () {
       this.addPage('campaigns');
 
       for (var campaign in levelRegistry.campaigns) {
@@ -77,14 +79,14 @@ define([
         console.log('adding', campaignId);
 
         this.map.campaigns.items.push({
-          text:campaignData.name,
-          message:'menu/show',
-          data:campaignId
+          text: campaignData.name,
+          message: 'menu/show',
+          data: campaignId
         });
 
         var item = this.map[campaignId] = {
-          node:null,
-          items:[]
+          node: null,
+          items: []
         };
 
         levelRegistry.getLevelIdsForCampaign(campaign).forEach(function (level) {
@@ -92,58 +94,58 @@ define([
           var levelData = levelRegistry.levels[level];
 
           item.items.push({
-            text:levelData.name,
-            message:'app/load',
+            text: levelData.name,
+            message: 'app/load',
             data: level
           });
 
         }, this);
 
         item.items.push({
-          text:'Back',
-          message:'menu/back'
+          text: 'Back',
+          message: 'menu/back'
         });
       }
 
       this.map.campaigns.items.push({
-        text:'Back',
-        message:'menu/back'
+        text: 'Back',
+        message: 'menu/back'
       });
     },
 
-    addPage: function(name, items){
+    addPage: function (name, items) {
       this.map[name] = {
         items: items || []
       };
     },
 
-    addItems: function(pageName, items){
-      if(!this.map[pageName]){
+    addItems: function (pageName, items) {
+      if (!this.map[pageName]) {
         return this.addPage(pageName, items);
       }
       this.map[pageName].push.apply(this.map[pageName], items);
     },
 
-    addItem: function(pageName, item){
-      if(!this.map[pageName]){
+    addItem: function (pageName, item) {
+      if (!this.map[pageName]) {
         return this.addPage(pageName, [item]);
       }
       this.map[pageName].push(item);
     },
 
-    show: function(name){
+    show: function (name) {
       this.history.push(name);
-      if(this.currentMenu){
+      if (this.currentMenu) {
         this.currentMenu.node.className = 'hidden';
       }
       this.currentMenu = this.map[name];
-      if(!this.currentMenu.node){
+      if (!this.currentMenu.node) {
         this.currentMenu.node = this.createMenuNode(this.map[name].items);
       }
       this.currentMenu.node.className = 'visible';
     },
 
-    createMenuNode: function(items){
+    createMenuNode: function (items) {
       var node = document.createElement('div');
       items.forEach(function(item){
         var itemNode = document.createElement('div');
@@ -151,7 +153,7 @@ define([
         itemNode.dataset.data = item.data;
         itemNode.className = 'menu-item';
         itemNode.innerHTML = item.text;
-        if(item.state){
+        if (item.state) {
           // need to fetch state from config
           var state = config.getItem(item.data);
           this.setState(itemNode, state ? 'on' : 'off');
@@ -162,13 +164,13 @@ define([
       return node;
     },
 
-    showLast: function(){
+    showLast: function () {
       this.history.pop();
       var last = this.history.pop();
       this.show(last);
     },
 
-    toggleState: function(node){
+    toggleState: function (node) {
       var map = ['on', 'off'];
       var state = node.dataset.state;
       var newState = map[(state == 'on') * 1];
@@ -178,7 +180,7 @@ define([
       comm.publish(node.dataset.topic, [node.dataset.data, newState]);
     },
 
-    setState: function(node, newState){
+    setState: function (node, newState) {
       var text = node.innerHTML.split(':')[0];
       node.innerHTML = text + ': ' + newState;
       node.dataset.state = newState;
